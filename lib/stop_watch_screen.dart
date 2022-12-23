38f70185b26021a29493b0fdb9a4423790944c34
yeonjoo.lee <teddyjune@naver.com>
2022-12-23 12:26:17 +0900

    버튼 3개 작성

---
+++ lib/stop_watch_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class StopWatchScreen extends StatefulWidget {
+  const StopWatchScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StopWatchScreen> createState() => _StopWatchScreenState();
+}
+
+class _StopWatchScreenState extends State<StopWatchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('스톱워치'),
+      ),
+      body: Column(
+        children: [
+          const Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FloatingActionButton(
+                backgroundColor: Colors.orange,
+                onPressed: () {},
+                child: const Icon(Icons.refresh),
+              ),
+              FloatingActionButton(
+                onPressed: () {},
+                child: const Icon(Icons.play_arrow),
+              ),
+              FloatingActionButton(
+                backgroundColor: Colors.green,
+                onPressed: () {},
+                child: const Icon(Icons.add),
+              ),
+            ],
+          ),
+          const SizedBox(height: 30),
+        ],
+      ),
+    );
+  }
+}
